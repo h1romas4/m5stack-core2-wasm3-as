670267db14eb13f124477a4689839d2d7c281147
h1romas4 <h1romas4@gmail.com>
2023-01-08 19:11:26 +0900

    fix web interface

--- wasm/imu6886/web/main.js
+++ wasm/imu6886/web/main.js
@@ -136,7 +136,7 @@ function decodeUTF8(wasmPtr) {
     await loadWasm();
     wasmExports.init(CANVAS_WIDTH, CANVAS_HEIGHT);
     setInterval(() => {
-        wasmExports.tick();
+        wasmExports.rotate();
         wasmExports.__collect() // clean up all garbage
     }, 16);
 })();
